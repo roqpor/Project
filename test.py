df50305e7eded707159742300f63795fd3c51d57
Sirinda22 <it58070149@office365.it.kmitl.ac.th>
2015-12-05 15:15:33 +0700

    graph
    test

---
+++ test.py
@@ -0,0 +1,19 @@
+from csv
+from IPyhon.core.display import HTML
+from nvd3 import multiBarChart
+chart = multiBarChart(width = 1024, heiht = 600, x_axis_format = None)
+data = []
+with open('csv.csv', newline = '', encoding = 'utf-8') as f:
+    reader = csv.reader(f)
+    for row in reader:
+        data.append(row)
+xdata = []
+ydata = []
+i = 0
+while i < len(data):
+    xdata.append(data[i][0])
+    ydata.append(int(data[i][5]))
+    i += 1
+chart.add_series(name = 'ENERGY', y = ydata, x = xdata)
+chart.buildhtml()
+HTML(chart.htmlcontent)
