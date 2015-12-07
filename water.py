b00183bc5fdbde6850cf371dc0210c69d3777357
Sirinda22 <it58070149@office365.it.kmitl.ac.th>
2015-12-07 19:51:31 +0700

    graph
    try to plot graph by details

--- water.py
+++ water.py
@@ -1,19 +1,51 @@
-import csv
-from IPyhon.core.display import HTML
-from nvd3 import multiBarChart
-chart = multiBarChart(width = 1024, heiht = 600, x_axis_format = None)
-data = []
-with open('csv.csv', newline = '', encoding = 'utf-8') as f:
-    reader = csv.reader(f)
-    for row in reader:
-        data.append(row)
-xdata = []
-ydata = []
-i = 0
-while i < len(data):
-    xdata.append(data[i][0])
-    ydata.append(int(data[i][5]))
-    i += 1
-chart.add_series(name = 'ENERGY', y = ydata, x = xdata)
-chart.buildhtml()
-HTML(chart.htmlcontent)
+def xxx():
+    import json
+    import csv
+    with open('csv.csv') as file:
+        stream = csv.reader(file)
+        coal = {}
+        gas = {}
+        water = {}
+        lst = ['jan', 'feb', 'mar', 'apr', 'may', 'june', 'july', 'aug', 'sep', 'oct', 'nov', 'dec', 'elect per year', 'price', 'capital']
+        for num, i in enumerate(stream):
+            if num == 0:
+                continue
+            if num == 1 or num == 2 or num == 3:
+                coal[i[1]] = dict()
+                for a, each in enumerate(lst):
+                    coal[i[1]][each] = i[a+2]
+            elif num == 4 or num == 5 or num == 6:
+                gas[i[1]] = dict()
+                for a, each in enumerate(lst):
+                    gas[i[1]][each] = i[a+2]
+            elif num == 7 or num == 8 or num == 9:
+                water[i[1]] = dict()
+                for a, each in enumerate(lst):
+                    water[i[1]][each] = i[a+2]
+    abc = coal['2551'].values()
+    abc = [float(i) for i in abc]
+    return abc
+print(xxx())
+
+
+        
+################################################################################
+
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+import plotly.plotly as py
+# Learn about API authentication here: https://plot.ly/python/getting-started
+# Find your api_key here: https://plot.ly/settings/api
+
+value = xxx()
+plt.hist(value)
+plt.title("stat")
+plt.xlabel("year")
+plt.ylabel("value")
+
+fig = plt.gcf()
+
+plt.show()
+
